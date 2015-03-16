e8dc5514179e8de3eca1d67065f58f7f8ee510d1
Hampus Nordin <nordin.hampus@gmail.com>
2015-03-16 12:47:27 +0100

    Documentation and minor code improvements for readability.

--- hampusn-current-template.php
+++ hampusn-current-template.php
@@ -3,7 +3,7 @@
 Plugin Name: Current Template
 Plugin URI: http://hampusnord.in/
 Description: This plugin adds the current template to the body and/or in the admin menu bar.
-Version: 0.0.0
+Version: 0.0.1
 Author: Hampus Nordin <hej@hampusnord.in>
 Author URI: http://hampusnord.in/
 Text Domain: hampusn_current_template
@@ -34,11 +34,15 @@ if (! class_exists('Hampusn_Current_Template_Admin')) {
 /**
  * Helper function which gets the currently stored settings from the database.
  *
+ * @param  boolean $reset Get fresh settings from the database.
  * @return array
  * @author Hampus Nordin
+ * @see    http://codex.wordpress.org/Function_Reference/get_option
  **/
 function hampusn_current_template_get_settings( $reset = false ) {
-  $hampusn_current_template_settings = array();
+  // Create a static cache so we only have to 
+  // call the database once per page load.
+  static $hampusn_current_template_settings = array();
   if ( empty( $hampusn_current_template_settings ) || true === $reset ) {
     $hampusn_current_template_settings = get_option( HAMPUSN_CURRENT_TEMPLATE_OPTION_NAME, array() );
   }
@@ -53,14 +57,22 @@ function hampusn_current_template_get_settings( $reset = false ) {
  * @author Hampus Nordin
  **/
 function hampusn_current_template_templates() {
+  // Create a static cache so this information isn't 
+  // processed more than once per page load.
   static $hampusn_current_template_templates = array();
   if ( empty( $hampusn_current_template_templates ) ) {
+    // All information about the current template 
+    // is stored in a global variable by WordPress.
     global $template;
+    // We shouldn't work with the global directly.
     $templates = $template;
+    // Make sure it's an array since we will assume it is 
+    // when we use it later.
     if ( ! is_array( $templates ) ) {
       $templates = array( $templates );
     }
-
+    // Remove the absolute path to the wordpress installation 
+    // from all templates since it's irrelevant.
     if ( defined( 'ABSPATH' ) ) {
       foreach ( $templates as &$tpl ) {
         $tpl = str_replace( ABSPATH, '', $tpl );
@@ -80,25 +92,30 @@ function hampusn_current_template_templates() {
  **/
 function hampusn_current_template_get_first_template() {
   $templates = hampusn_current_template_templates();
-  return $templates[ 0 ];
+  return ! empty( $templates ) ? $templates[ 0 ] : '';
 }
 
 
 /**
  * WP hook callback which adds css and js.
+ * 
+ * Action hook: wp_enqueue_scripts
  *
  * @return void
  * @author Hampus Nordin
+ * @see    http://codex.wordpress.org/Plugin_API/Action_Reference/wp_enqueue_scripts
  **/
 function hampusn_current_template_add_css_js() {
-  $settings = hampusn_current_template_get_settings();
-
-  wp_enqueue_style( 'hampusn-current-template-css', plugins_url( 'hampusn-current-template.css', __FILE__ ), array(), '0.0.0', 'screen' );
-
-  // So far, the js file is only used for printing the modal box. 
-  // So, no need to include anything if the modal shouldn't even be printed.
-  if ( ! empty( $settings[ 'show_modal_in_body_top' ] ) ) {
-    wp_enqueue_script( 'hampusn-current-template-js', plugins_url( 'hampusn-current-template.js', __FILE__ ), array( 'jquery' ), '0.0.0', true );
+  // Only enqueue styling and script if user is logged in.
+  if ( is_user_logged_in() ) {
+    // Enqueue styling
+    wp_enqueue_style( 'hampusn-current-template-css', plugins_url( 'hampusn-current-template.css', __FILE__ ), array(), '0.0.1', 'screen' );
+    // So far, the js file is only used for printing the modal box. 
+    // So, no need to include anything if the modal shouldn't even be printed.
+    $settings = hampusn_current_template_get_settings();
+    if ( ! empty( $settings[ 'show_modal_in_body_top' ] ) ) {
+      wp_enqueue_script( 'hampusn-current-template-js', plugins_url( 'hampusn-current-template.js', __FILE__ ), array( 'jquery' ), '0.0.0', true );
+    }
   }
 }
 add_action( 'wp_enqueue_scripts', 'hampusn_current_template_add_css_js' );
@@ -106,31 +123,38 @@ add_action( 'wp_enqueue_scripts', 'hampusn_current_template_add_css_js' );
 
 /**
  * WP hook callback which exposes js variables.
+ * 
+ * Action hook: wp_head
  *
  * @return void
  * @author Hampus Nordin
+ * @see    http://codex.wordpress.org/Plugin_API/Action_Reference/wp_head
  **/
 function hampusn_current_template_wp_head() {
   $settings = hampusn_current_template_get_settings();
   // So far, the js file is only used for printing the modal box. 
   // So, no need to include anything if the modal shouldn't even be printed.
   if ( ! empty( $settings[ 'show_modal_in_body_top' ] ) ) {
+    // Pass variables to frontend for js usage.
     wp_localize_script( 'hampusn-current-template-js', 'hampusnCurrentTemplate', array(
       'templates' => hampusn_current_template_templates(),
       'isAdmin' => (int) current_user_can( 'install_themes' ),
       'showModalInBodyTop' => ! empty( $settings[ 'show_modal_in_body_top' ] ) ? 1 : 0,
     ) );
   }
-
 }
 add_action( 'wp_head', 'hampusn_current_template_wp_head' );
 
 
 /**
  * WP hook callback which adds the template to the admin menu bar.
+ * 
+ * Action hook: admin_bar_menu
  *
  * @return void
  * @author Hampus Nordin
+ * @see    http://codex.wordpress.org/Plugin_API/Action_Reference/admin_bar_menu
+ * @see    http://codex.wordpress.org/Class_Reference/WP_Admin_Bar
  **/
 function hampusn_current_template_modify_admin_bar( $wp_admin_bar )  {
   // Get the first template if one exist.
@@ -141,25 +165,29 @@ function hampusn_current_template_modify_admin_bar( $wp_admin_bar )  {
   if ( $current_template && ! empty( $settings[ 'show_in_admin_menu' ] ) ) {
     // Get a list of all current templates.
     $templates = hampusn_current_template_templates();
+    // Get the filename from the current template.
+    $filename = basename( $current_template );
     // Add the main menu item which will show only the filename of the current template.
     $wp_admin_bar->add_menu( array(
       'id' => 'hampusn-current-template',
-      'title' => 'Current Template: <strong>' . basename( $current_template ) . '</strong>',
+      'title' => "Current Template: <strong>{$filename}</strong>",
       'parent' => false,
       'href' => false,
       'group' => false,
       'meta' => array(
         'class' => 'hampusn-current-template-admin-menu-item',
-        'html' => $sub_menu,
       ),
     ) );    
     // Add the rest of the templates (including the first one) with full path.
     foreach ( $templates as $key => $tpl ) {
-      $tpl_parts = explode( '/', $tpl );
-      $filename = array_pop( $tpl_parts );
+      // Split up the template path into filename and 
+      // filepath for the specific html markup below.
+      $filename = basename( $tpl );
+      $filepath = dirname( $tpl );
+      // Add template to menu.
       $wp_admin_bar->add_node( array(
         'id' => 'hampusn-current-template-tpl-' . $key,
-        'title' => "<strong>{$key}:</strong> <span>" . implode( '/', $tpl_parts ) . "/</span><strong>{$filename}</strong>",
+        'title' => "<strong>{$key}:</strong> <span>{$filepath}/</span><strong>{$filename}</strong>",
         'parent' => 'hampusn-current-template',
       ) );
     }
